c94f7409168eacd46a7c129890b5389161570128
Andrey Fedorushkov <andrf@mail.ru>
2013-11-04 14:07:48 +0400

    fix build-version

--- common/build_version.cpp
+++ common/build_version.cpp
@@ -7,7 +7,7 @@
 
 #ifndef KICAD_BUILD_VERSION
 #if defined KICAD_GOST
-#   define KICAD_BUILD_VERSION "(2013-oct-26 BZR 4221 GOST-COMMITTERS (bzr4415-testing))"
+#   define KICAD_BUILD_VERSION "(2013-oct-27 BZR 4222 GOST-COMMITTERS (bzr4415-testing))"
 #else
 #   define KICAD_BUILD_VERSION "(2013-jul-14)"
 #endif
